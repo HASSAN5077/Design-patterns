c26e6828dc951f5aca734f92d0cb499970629bcf
MUHAMMAD HASSAN NAWAZ <muhammadhassanm531@gmail.com>
2023-01-06 20:06:50 +0500

    abstraction pattern

---
+++ src/AbstractionOccurence/Book.java
@@ -0,0 +1,9 @@
+package AbstractionOccurence;
+
+public class Book extends BookAbstraction{
+    protected String barCodeNumber;
+    @Override
+    String getName() {
+        return null;
+    }
+}
---
+++ src/AbstractionOccurence/BookAbstraction.java
@@ -0,0 +1,10 @@
+package AbstractionOccurence;
+
+public abstract class BookAbstraction {
+    protected String name;
+    protected String author;
+    protected String isbn;
+    protected String date;
+
+    abstract String getName();
+}
